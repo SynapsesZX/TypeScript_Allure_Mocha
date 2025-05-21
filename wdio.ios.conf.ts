477ba70c41d055f6d0dc4a8cd5a6cda582cb3ce7
igoroppizi <igor@oppizi.com>
2025-05-21 20:15:12 +0300

    dfd

--- wdio.ios.conf.ts
+++ wdio.ios.conf.ts
@@ -2,7 +2,7 @@ export const config: WebdriverIO.Config = {
   runner: 'local',
   tsConfigPath: './tsconfig.json',
 
-  hostname: '192.168.0.107',
+  hostname: '192.168.0.106',
   port: 4723,
   path: '/',
 
